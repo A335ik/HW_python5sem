c4fde1129d09cb85c68aaeda0f894090915c09fb
A335ik <56710180+A335ik@users.noreply.github.com>
2024-10-06 23:05:00 +0300

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,5 +1,44 @@
 import math
+class Complex:
  
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+ 
+    def __repr__(self):
+        return f'{self.a}{"-" if self.b < 0 else "+"}{abs(self.b)}j'
+ 
+    def __add__(self, other):
+        if isinstance(other, Complex):
+            return Complex(self.a + other.a, self.b + other.b)
+        if isinstance(other, (int, float)):
+            return Complex(self.a + other, self.b)
+        raise NotImplementedError
+ 
+    def __sub__(self, other):
+        if isinstance(other, Complex):
+            return Complex(self.a - other.a, self.b - other.b)
+        if isinstance(other, (int, float)):
+            return Complex(self.a - other, self.b)
+        raise NotImplementedError
+ 
+    def __mul__(self, other):
+        if isinstance(other, Complex):
+            return Complex(self.a*other.a - self.b*other.b, self.a*other.b + self.b*other.a)
+        if isinstance(other, (int, float)):
+            return Complex(self.a*other, self.b*other)
+        raise NotImplementedError
+ 
+    def __truediv__(self, other):
+        ...
+ 
+ 
+c1 = Complex(1, 2)
+c2 = Complex(7, -8)
+print(c1, c2)
+print(c1 + c2)
+
+
 class Complex:
     
     def __init__(self,r,a):
